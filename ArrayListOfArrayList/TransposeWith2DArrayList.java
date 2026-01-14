5ab9a4ea6a03d09ce5882c53ff1fc7fe43950165
Vinay <pandey57vinay@gmail.com>
2026-01-14 22:19:10 +0530

    Added proper printing process in the Transpose Matric with 2D ArrayList

--- ArrayListOfArrayList/TransposeWith2DArrayList.java
+++ ArrayListOfArrayList/TransposeWith2DArrayList.java
@@ -5,8 +5,24 @@ import java.util.ArrayList;
 //this program is to find the transpose of a matrix but using array lists
 public class TransposeWith2DArrayList {
     public static void main(String[] args) {
-        int arr[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
-        System.out.println(transpose(arr));
+        int arr[][] = {{1,2,3,8}, {7,4,5,6}, {7,8,9,6}};
+        for(int i=0; i< arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("-----------");
+        ArrayList<ArrayList<Integer>> a = new ArrayList<>();
+        a = transpose(arr);
+
+        for(int i=0; i<a.size(); i++){
+            for(int j=0; j<a.get(0).size(); j++){
+                System.out.print(a.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
     }
     public static ArrayList<ArrayList<Integer>> transpose(int[][] mat) {
 
@@ -30,7 +46,6 @@ public class TransposeWith2DArrayList {
                 arr.get(j).add(mat[i][j]);
             }
         }
-
         return arr;
     }
 }
